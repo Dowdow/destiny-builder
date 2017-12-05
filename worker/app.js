706799697e86e0ebf079b0eb474aee21c40ce479
Léo Riera <leo.riera1@gmail.com>
2017-12-05 13:38:06 +0100

    Initial commit

---
+++ worker/app.js
@@ -0,0 +1,34 @@
+const mongo = require('mongodb');
+
+mongo.MongoClient.connect('mongodb://localhost/destiny', (err, db) => {
+    if (err) throw err;
+
+    function getItems() {
+
+    }
+
+    function createTitanGroups() {
+
+    }
+
+    function createHunterGroups() {
+
+    }
+
+    function createWarlockGroups() {
+
+    }
+
+    function createTitanBuilds() {
+
+    }
+
+    function createHunterBuilds() {
+
+    }
+
+    function createWarlockBuilds() {
+
+    }
+
+});  
