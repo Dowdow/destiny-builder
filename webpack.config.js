b206d6be435a5308ee1dd193d4d499046872a138
Léo Riera <leo.riera1@gmail.com>
2017-12-08 09:41:44 +0100

    Lint webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const BUILD_DIR = path.resolve(__dirname, "client/build");
-const PUBLIC_DIR = path.resolve(__dirname, "client/public");
-const SRC_DIR = path.resolve(__dirname, "client/src");
+const BUILD_DIR = path.resolve(__dirname, 'client/build');
+const PUBLIC_DIR = path.resolve(__dirname, 'client/public');
+const SRC_DIR = path.resolve(__dirname, 'client/src');
 
-let isDev = process.argv.indexOf("-p") !== -1 ? false : true;
+const isDev = process.argv.indexOf('-p') === -1;
 
 module.exports = {
   entry: {
-    javascript: SRC_DIR + "/index.js"
+    javascript: `${SRC_DIR}/index.js`,
   },
   output: {
     path: BUILD_DIR,
-    filename: `bundle${isDev ? "" : ".[chunkhash]"}.js`
+    filename: `bundle${isDev ? '' : '.[chunkhash]'}.js`,
   },
   devtool: isDev ? 'eval-source-map' : 'none',
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: ['.js', '.jsx'],
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ["babel-loader"]
-      }
-    ]
+        loaders: ['babel-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: `${PUBLIC_DIR}/index.html`,
-      filename: "index.html",
-      inject: "body"
-    })
-  ]
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
 };
