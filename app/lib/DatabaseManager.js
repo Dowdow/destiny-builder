01229f5181948de75a2442d1249cf600f5784668
Léo Riera <leo.riera1@gmail.com>
2017-12-12 15:27:32 +0100

    Mongoose schema and model

--- app/lib/DatabaseManager.js
+++ app/lib/DatabaseManager.js
@@ -2,6 +2,106 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
+/** Stat Schema */
+const statSchema = new mongoose.Schema({
+  descriptions: {
+    de: String,
+    en: String,
+    'es-mx': String,
+    es: String,
+    fr: String,
+    it: String,
+    ja: String,
+    pl: String,
+    'pt-br': String,
+    ru: String,
+    'zh-cht': String,
+  },
+  hash: String,
+  img: String,
+  names: {
+    de: String,
+    en: String,
+    'es-mx': String,
+    es: String,
+    fr: String,
+    it: String,
+    ja: String,
+    pl: String,
+    'pt-br': String,
+    ru: String,
+    'zh-cht': String,
+  },
+});
+
+/** Mod Schema */
+const modSchema = new mongoose.Schema({
+  descriptions: {
+    de: String,
+    en: String,
+    'es-mx': String,
+    es: String,
+    fr: String,
+    it: String,
+    ja: String,
+    pl: String,
+    'pt-br': String,
+    ru: String,
+    'zh-cht': String,
+  },
+  hash: String,
+  img: String,
+  names: {
+    de: String,
+    en: String,
+    'es-mx': String,
+    es: String,
+    fr: String,
+    it: String,
+    ja: String,
+    pl: String,
+    'pt-br': String,
+    ru: String,
+    'zh-cht': String,
+  },
+  type: String,
+});
+
+/** Armor Schema */
+const armorSchema = new mongoose.Schema({
+  defense: Number,
+  descriptions: {
+    de: String,
+    en: String,
+    'es-mx': String,
+    es: String,
+    fr: String,
+    it: String,
+    ja: String,
+    pl: String,
+    'pt-br': String,
+    ru: String,
+    'zh-cht': String,
+  },
+  hash: String,
+  img: String,
+  names: {
+    de: String,
+    en: String,
+    'es-mx': String,
+    es: String,
+    fr: String,
+    it: String,
+    ja: String,
+    pl: String,
+    'pt-br': String,
+    ru: String,
+    'zh-cht': String,
+  },
+  power: Number,
+  screenshot: String,
+});
+
 /**
  * Represents the Manager for our Databases.
  * This class exposes methods to write, delete and modify data
@@ -14,6 +114,15 @@ class DatabaseManager {
    */
   constructor({ mongoURL }) {
     this.mongoURL = mongoURL || process.env.MONGO_URL;
+
+    /** Stat Model */
+    this.Stat = mongoose.model('Stat', statSchema);
+
+    /** Mod Model */
+    this.Mod = mongoose.model('Mod', modSchema);
+
+    /** Armor Model */
+    this.Armor = mongoose.model('Armor', armorSchema);
   }
 
   connectMongo() {
@@ -26,8 +135,109 @@ class DatabaseManager {
       });
   }
 
+  disconnectMongo() {
+    return mongoose.disconnect().then(() => {
+      console.log('Mongo disconnected.');
+    });
+  }
+
   connect() {
     return Promise.all([this.connectMongo()]);
+  }
+
+  disconnect() {
+    return Promise.all([this.disconnectMongo()]);
+  }
+
+  /**
+   * Save and array of Stat
+   * @param {Array} stats
+   */
+  saveStats(stats) {
+    return new Promise((resolve, reject) => {
+      const promises = [];
+      stats.forEach((element) => {
+        promises.push(this.saveStat(element));
+      });
+      Promise.all(promises)
+        .then(() => resolve())
+        .catch(err => reject(err));
+    });
+  }
+
+  /**
+   * Save a Stat object
+   * @param {Stat} stat
+   */
+  saveStat(stat) {
+    return new Promise((resolve, reject) => {
+      const s = new this.Stat(stat);
+      s.save((err, res) => {
+        if (err) reject(err);
+        resolve(res);
+      });
+    });
+  }
+
+  /**
+   * Save an array of Mod
+   * @param {Array} mods
+   */
+  saveMods(mods) {
+    return new Promise((resolve, reject) => {
+      const promises = [];
+      mods.forEach((element) => {
+        promises.push(this.saveMod(element));
+      });
+      Promise.all(promises)
+        .then(() => resolve())
+        .catch(err => reject(err));
+    });
+  }
+
+  /**
+   * Save a Mod object
+   * @param {Mod} mod
+   */
+  saveMod(mod) {
+    return new Promise((resolve, reject) => {
+      const m = new this.Mod(mod);
+      m.save((err, res) => {
+        if (err) reject(err);
+        resolve(res);
+      });
+    });
+  }
+
+  /**
+   * Save an array of Armor
+   * @param {Array} armors
+   */
+  saveArmors(armors) {
+    console.log(armors);
+    return new Promise((resolve, reject) => {
+      const promises = [];
+      armors.forEach((element) => {
+        promises.push(this.saveArmor(element));
+      });
+      Promise.all(promises)
+        .then(() => resolve())
+        .catch(err => reject(err));
+    });
+  }
+
+  /**
+   * Save a Armor object
+   * @param {Armor} armor
+   */
+  saveArmor(armor) {
+    return new Promise((resolve, reject) => {
+      const a = new this.Armor(armor);
+      a.save((err, res) => {
+        if (err) reject(err);
+        resolve(res);
+      });
+    });
   }
 }
 
--- app/worker/items.js
+++ app/worker/items.js
@@ -25,146 +25,184 @@ const mods = [];
 const armors = [];
 
 function readStatFile(file, lang) {
-  fs.readFile(`${STAT_DIR}/${file}`, (err, data) => {
-    if (err) throw err;
-    const json = JSON.parse(data);
-    Object.keys(json).forEach((id) => {
-      const stat = json[id];
-      if (stat.displayProperties.name !== undefined) {
-        const existingStat = stats.find(element => element.id === id);
-        if (existingStat) {
-          existingStat.names[lang] = stat.displayProperties.name;
-          existingStat.descriptions[lang] = stat.displayProperties.description;
-        } else {
-          const obj = {
-            id,
-            names: {
-              [lang]: stat.displayProperties.name,
-            },
-            descriptions: {
-              [lang]: stat.displayProperties.description,
-            },
-          };
-          if (stat.displayProperties.hasIcon) {
-            obj.img = `${BUNGIE_ROOT}${stat.displayProperties.icon}`;
+  return new Promise((resolve, reject) => {
+    fs.readFile(`${STAT_DIR}/${file}`, (err, data) => {
+      if (err) reject(err);
+      const json = JSON.parse(data);
+      Object.keys(json).forEach((id) => {
+        const stat = json[id];
+        if (stat.displayProperties.name !== undefined) {
+          const existingStat = stats.find(element => element.hash === id);
+          if (existingStat) {
+            existingStat.names[lang] = stat.displayProperties.name;
+            existingStat.descriptions[lang] = stat.displayProperties.description;
+          } else {
+            const obj = {
+              hash: id,
+              names: {
+                [lang]: stat.displayProperties.name,
+              },
+              descriptions: {
+                [lang]: stat.displayProperties.description,
+              },
+            };
+            if (stat.displayProperties.hasIcon) {
+              obj.img = `${BUNGIE_ROOT}${stat.displayProperties.icon}`;
+            }
+            stats.push(obj);
           }
-          stats.push(obj);
         }
-      }
+      });
+      resolve();
     });
   });
 }
 
 function readModFile(file, lang) {
-  fs.readFile(`${MOD_DIR}/${file}`, (err, data) => {
-    if (err) throw err;
-    const json = JSON.parse(data);
-    Object.keys(json).forEach((id) => {
-      const mod = json[id];
-      if (mod.displayProperties.name !== undefined) {
-        const existingMod = mods.find(element => element.id === id);
-        if (existingMod) {
-          existingMod.names[lang] = mod.displayProperties.name;
-          existingMod.descriptions[lang] = mod.displayProperties.description;
-        } else {
-          const obj = {
-            id,
-            names: {
-              [lang]: mod.displayProperties.name,
-            },
-            descriptions: {
-              [lang]: mod.displayProperties.description,
-            },
-          };
-          if (mod.displayProperties.hasIcon) {
-            obj.img = `${BUNGIE_ROOT}${mod.displayProperties.icon}`;
+  return new Promise((resolve, reject) => {
+    fs.readFile(`${MOD_DIR}/${file}`, (err, data) => {
+      if (err) reject(err);
+      const json = JSON.parse(data);
+      Object.keys(json).forEach((id) => {
+        const mod = json[id];
+        if (mod.displayProperties.name !== undefined) {
+          const existingMod = mods.find(element => element.hash === id);
+          if (existingMod) {
+            existingMod.names[lang] = mod.displayProperties.name;
+            existingMod.descriptions[lang] = mod.displayProperties.description;
+          } else {
+            const obj = {
+              hash: id,
+              names: {
+                [lang]: mod.displayProperties.name,
+              },
+              descriptions: {
+                [lang]: mod.displayProperties.description,
+              },
+            };
+            if (mod.displayProperties.hasIcon) {
+              obj.img = `${BUNGIE_ROOT}${mod.displayProperties.icon}`;
+            }
+            if (mod.plug.plugCategoryIdentifier !== undefined) {
+              obj.type = mod.plug.plugCategoryIdentifier;
+            }
+            mods.push(obj);
           }
-          if (mod.plug.plugCategoryIdentifier !== undefined) {
-            obj.type = mod.plug.plugCategoryIdentifier;
-          }
-          mods.push(obj);
         }
-      }
+      });
+      resolve();
     });
   });
 }
 
 function readArmorFile(file, lang) {
-  fs.readFile(`${ARMOR_DIR}/${file}`, (err, data) => {
-    if (err) throw err;
-    const json = JSON.parse(data);
-    Object.keys(json).forEach((id) => {
-      const armor = json[id];
-      if (armor.displayProperties.name !== undefined && armor.inventory.tierType > 4) {
-        const existingArmor = armors.find(element => element.id === id);
-        if (existingArmor) {
-          existingArmor.names[lang] = armor.displayProperties.name;
-          existingArmor.descriptions[lang] = armor.displayProperties.description;
-        } else {
-          const obj = {
-            id,
-            names: {
-              [lang]: armor.displayProperties.name,
-            },
-            descriptions: {
-              [lang]: armor.displayProperties.description,
-            },
-          };
-          if (armor.displayProperties.hasIcon) {
-            obj.img = `${BUNGIE_ROOT}${armor.displayProperties.icon}`;
-          }
-          if (armor.screenshot !== undefined) {
-            obj.screenshot = `${BUNGIE_ROOT}${armor.screenshot}`;
-          }
-          if (armor.sourceData !== undefined && armor.sourceData.sources !== undefined) {
-            obj.defence = 0;
-            obj.power = 0;
-            armor.sourceData.sources.forEach((level) => {
-              if (level.computedStats[3897883278] !== undefined) {
-                if (level.computedStats[3897883278].value > obj.defence) {
-                  obj.defence = level.computedStats[3897883278].value;
+  return new Promise((resolve, reject) => {
+    fs.readFile(`${ARMOR_DIR}/${file}`, (err, data) => {
+      if (err) reject(err);
+      const json = JSON.parse(data);
+      Object.keys(json).forEach((id) => {
+        const armor = json[id];
+        if (armor.displayProperties.name !== undefined && armor.inventory.tierType > 4) {
+          const existingArmor = armors.find(element => element.hash === id);
+          if (existingArmor) {
+            existingArmor.names[lang] = armor.displayProperties.name;
+            existingArmor.descriptions[lang] = armor.displayProperties.description;
+          } else {
+            const obj = {
+              hash: id,
+              names: {
+                [lang]: armor.displayProperties.name,
+              },
+              descriptions: {
+                [lang]: armor.displayProperties.description,
+              },
+            };
+            if (armor.displayProperties.hasIcon) {
+              obj.img = `${BUNGIE_ROOT}${armor.displayProperties.icon}`;
+            }
+            if (armor.screenshot !== undefined) {
+              obj.screenshot = `${BUNGIE_ROOT}${armor.screenshot}`;
+            }
+            if (armor.sourceData !== undefined && armor.sourceData.sources !== undefined) {
+              obj.defense = 0;
+              obj.power = 0;
+              armor.sourceData.sources.forEach((level) => {
+                if (level.computedStats[3897883278] !== undefined) {
+                  if (level.computedStats[3897883278].value > obj.defense) {
+                    obj.defense = level.computedStats[3897883278].value;
+                  }
                 }
-              }
-              if (level.computedStats[1935470627] !== undefined) {
-                if (level.computedStats[1935470627].value > obj.power) {
-                  obj.power = level.computedStats[1935470627].value;
+                if (level.computedStats[1935470627] !== undefined) {
+                  if (level.computedStats[1935470627].value > obj.power) {
+                    obj.power = level.computedStats[1935470627].value;
+                  }
                 }
-              }
-            });
+              });
+            }
+            armors.push(obj);
           }
-          armors.push(obj);
         }
-      }
+      });
+      resolve();
     });
   });
 }
 
 module.exports = {
-  saveStats: () => {
+  saveStats: () => new Promise((resolve, reject) => {
     fs.readdir(STAT_DIR, (err, files) => {
-      if (err) throw err;
+      if (err) reject(err);
+      const promises = [];
       files.forEach((file) => {
         const lang = file.split('.')[0];
-        readStatFile(file, lang);
+        promises.push(readStatFile(file, lang));
+      });
+      Promise.all(promises).then(() => {
+        dbManager.saveStats(stats)
+          .then(() => {
+            resolve();
+          })
+          .catch((pErr) => {
+            console.log(`An error occured while saving the stats ${pErr}`);
+          });
       });
     });
-  },
-  saveMods: () => {
+  }),
+  saveMods: () => new Promise((resolve, reject) => {
     fs.readdir(MOD_DIR, (err, files) => {
-      if (err) throw err;
+      if (err) reject(err);
+      const promises = [];
       files.forEach((file) => {
         const lang = file.split('.')[0];
-        readModFile(file, lang);
+        promises.push(readModFile(file, lang));
+      });
+      Promise.all(promises).then(() => {
+        dbManager.saveMods(mods)
+          .then(() => {
+            resolve();
+          }).catch((pErr) => {
+            console.log(`An error occured while saving the mods ${pErr}`);
+          });
       });
     });
-  },
-  saveArmors: () => {
+  }),
+  saveArmors: () => new Promise((resolve, reject) => {
     fs.readdir(ARMOR_DIR, (err, files) => {
-      if (err) throw err;
+      if (err) reject(err);
+      const promises = [];
       files.forEach((file) => {
         const lang = file.split('.')[0];
-        readArmorFile(file, lang);
+        promises.push(readArmorFile(file, lang));
+      });
+      Promise.all(promises).then(() => {
+        dbManager.saveArmors(armors)
+          .then(() => {
+            dbManager.disconnect();
+            resolve();
+          }).catch((pErr) => {
+            console.log(`An error occured while saving the armors ${pErr}`);
+          });
       });
     });
-  },
+  }),
 };
