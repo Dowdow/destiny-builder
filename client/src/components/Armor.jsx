f164a5c137c3e5be2947882bc6b601597894e495
Léo Riera <leo.riera1@gmail.com>
2017-12-20 18:07:06 +0100

    Modal basics

--- client/src/components/Armor.jsx
+++ client/src/components/Armor.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/Armor.css';
 
-const Armor = props => (
-  <div className="Armor">
-    <img src={props.img} alt={props.names.fr} />
-  </div>
-);
+class Armor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      show: !prevState.show,
+    }));
+  }
+
+  render() {
+    return (
+      <div className="Armor">
+        <img src={this.props.img} alt={this.props.names.fr} onClick={this.handleClick} />
+        {this.state.show ?
+          <div className="ArmorModal" onClick={this.handleClick}>Modal</div>
+        : ''}
+      </div>
+    );
+  }
+}
 
 export default Armor;
