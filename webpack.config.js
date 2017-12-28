929b04680899a6569b8db0546298528c9b668e0e
Léo Riera <leo.riera1@gmail.com>
2017-12-28 17:58:34 +0100

    Webpack clean plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const BUILD_DIR = path.resolve(__dirname, 'client/build');
 const PUBLIC_DIR = path.resolve(__dirname, 'client/public');
@@ -44,11 +45,19 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(BUILD_DIR),
     new HtmlWebpackPlugin({
       template: `${PUBLIC_DIR}/index.html`,
       filename: 'index.html',
       inject: 'body',
       favicon: `${PUBLIC_DIR}/favicon.png`,
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false,
+      compress: {
+        drop_debugger: true,
+        drop_console: true,
+      },
     }),
   ],
 };
