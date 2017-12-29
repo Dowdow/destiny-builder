a5ff73f1cd6ad376d0295eed3edb09423a5e478a
Léo Riera <leo.riera1@gmail.com>
2017-12-29 12:46:08 +0100

    Google Analytics + fix api hostname in client

--- client/src/api.js
+++ client/src/api.js
@@ -1,10 +1,16 @@
-const host = 'http://localhost:3000';
+const HOSTNAME = window && window.location && window.location.hostname;
+
+let HOST = `http://${HOSTNAME}`;
+
+if (HOSTNAME === 'localhost') {
+  HOST = `${HOST}:3000`;
+}
 
 async function callApi(route, filters) {
   return new Promise(async (resolve, reject) => {
     try {
       const query = Object.keys(filters).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(filters[k])}`).join('&');
-      const res = await fetch(`${host}/${route}?${query}`);
+      const res = await fetch(`${HOST}/${route}?${query}`);
       if (res.status === 200) {
         const json = await res.json();
         resolve(json);
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import App from './components/App';
 import './css/index.css';
 
+ReactGA.initialize('UA-111738679-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.render(<App />, document.getElementById('root'));
